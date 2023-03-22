2af699649b6c77f56b36f18a3829fe8395572981
HuyC <huy.com.hg@gmail.com>
2023-03-23 06:56:58 +0700

    Update teacherController

---
+++ FinalProject/API/Controllers/ClassController.cs
@@ -0,0 +1,53 @@
+﻿using API.Models;
+using API.Profiles;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private Prn231dbContext dbContext = new Prn231dbContext();
+        private readonly IMapper mapper;
+        public ClassController()
+        {
+            var config = new MapperConfiguration(cf => cf.AddProfile(new ClassProfile())); ;
+            mapper = config.CreateMapper();
+        }
+        
+        [HttpGet]
+        public IActionResult getClass()
+        {
+            List<Class> cList = dbContext.Classes
+                .Include(o => o.Teacher).ToList();
+            List<ClassDTO> classesList = new List<ClassDTO>();
+            foreach(Class item in cList)
+            {
+                classesList.Add(mapper.Map<ClassDTO>(item));
+            }
+            classesList = cList.Select(mapper.Map<Class, ClassDTO>).ToList();
+            return Ok(classesList);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult getStudentsByClassId(int id)
+        {
+            var studentList = dbContext.ClassStudents.
+                Where(c => c.ClassId == id).Select(c => new
+            {
+                studentId = c.StudentId,
+                studentName = c.Student.Name,
+                /*Account = c.Student.Accounts.
+                Select(s => new
+                {
+                    email = s.Email
+                })*/
+                }
+            );
+            return Ok(studentList);
+        }
+    }
+}
---
+++ FinalProject/API/Controllers/TeacherController.cs
@@ -0,0 +1,29 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private Prn231dbContext dbContext = new Prn231dbContext();
+
+        [HttpGet]
+        public IActionResult getTeachers()
+        {
+            return Ok(dbContext.Teachers.ToList());
+        }
+        /*[HttpPost]
+        public IActionResult addTeacher(Teacher t)
+        {
+            var teacher = new Teacher()
+            {
+                Name = t.Name
+            };
+            dbContext.Teachers.Add(teacher);
+            dbContext.SaveChanges();
+            return Ok(teacher);
+        }*/
+    }
+}
