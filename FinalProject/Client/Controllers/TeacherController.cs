1e0026f4d817ce90587c44f08416c417d3b7e9ba
HuyC <huy.com.hg@gmail.com>
2023-03-23 23:25:30 +0700

    Update TeacherController.cs

--- FinalProject/Client/Controllers/TeacherController.cs
+++ FinalProject/Client/Controllers/TeacherController.cs
@@ -1,11 +1,21 @@
 ﻿using Client.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace Client.Controllers
 {
 	public class TeacherController : Controller
 	{
+		private readonly HttpClient client = null;
+		private string teacherApiUrl = "";
+		public TeacherController()
+		{
+			client = new HttpClient();
+			var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+			client.DefaultRequestHeaders.Accept.Add(contentType);
+			teacherApiUrl = "http://localhost:5143/api/Teacher";
+        }
 		Teacher? GetTeacherFromSession()
 		{
 			string str = HttpContext.Session.GetString("AccountSession");
@@ -15,8 +25,16 @@ namespace Client.Controllers
 			Teacher teacher = new Prn231dbContext().Teachers.SingleOrDefault(t => t.Id == account.Id);
 			return teacher;
 		}
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
+			HttpResponseMessage response = await client.GetAsync(teacherApiUrl);
+			string strData = await response.Content.ReadAsStringAsync();
+
+			var options = new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			};
+			List<Teacher> teachers = JsonSerializer.Deserialize<List<Teacher>>(strData, options);
 			Teacher? teacher = GetTeacherFromSession();
 			if (teacher == null)
 				return RedirectToAction("Index", "SignIn");
