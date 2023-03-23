5e6317e5f3e904eae44124740dd1da2e754212ce
HuyC <huy.com.hg@gmail.com>
2023-03-23 22:02:08 +0700

    Update AccountController.cs

--- FinalProject/API/Controllers/AccountController.cs
+++ FinalProject/API/Controllers/AccountController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController : Controller
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
-        public IActionResult Index()
+        private Prn231dbContext dbContext = new Prn231dbContext();
+
+        [HttpGet]
+        [Route("{email}")]
+        public IActionResult GetAccountByEmail(string email)
         {
-            return View();
+            var account = dbContext.Accounts
+                .SingleOrDefault(a => a.Email.Equals(email));
+            return Ok(account);
+        }
+        [HttpGet]
+        public IActionResult GetStudentByAccountId(int id)
+        {
+            var account = dbContext.Accounts.SingleOrDefault(a => a.Id == id);
+            var student = dbContext.Students
+                .Select(s => new Student
+                {
+                    Id = s.Id,
+                    Name = s.Name
+                })
+                .SingleOrDefault(s => s.Id.Equals(account.StudentId))
+                ;
+            return Ok(student);
         }
     }
 }
